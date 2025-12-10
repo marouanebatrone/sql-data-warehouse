c62f23b752bc0999bd8c1a7f2eaf9e18bf2f13f4
marouane batrone <batrone.marouane@gmail.com>
2025-12-11 00:02:04 +0100

    crm tables of silver layer

--- scripts/silver/load_silver.sql
+++ scripts/silver/load_silver.sql
@@ -66,3 +66,39 @@ FROM bronze.crm_prd_info;
 
 
 
+-- sales_details
+INSERT INTO silver.crm_sales_details (
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	sls_order_dt,
+	sls_ship_dt,
+	sls_due_dt,
+	sls_sales,
+	sls_quantity,
+	sls_price
+)
+SELECT
+sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+CASE WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
+	 ELSE CAST(CAST(sls_order_dt AS NVARCHAR) AS DATE)
+END AS sls_order_dt,
+CASE WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL
+			ELSE CAST(CAST(sls_ship_dt AS NVARCHAR) AS DATE)
+END AS sls_ship_dt,
+CASE WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL
+			ELSE CAST(CAST(sls_due_dt AS NVARCHAR) AS DATE)
+END AS sls_due_dt,
+CASE WHEN sls_sales IS NULL OR sls_sales <= 0 THEN sls_price / NULLIF(sls_quantity, 0)
+	 ELSE sls_sales 
+END AS sls_sales,
+sls_quantity,
+CASE WHEN sls_price IS NULL OR sls_price <= 0 THEN sls_quantity * ABS(sls_sales)
+	 ELSE sls_price 
+END AS sls_price
+FROM bronze.crm_sales_details
+
+
+
